ba3cf60db5cbf6a32069414a183a57f3b3187b9d
Bhaskar <bhaskar.bid18@gmail.com>
2025-02-08 02:23:35 +0530

    Successful run on http://127.0.0.1:8000

--- AISamacharapp/AISamacharapp/settings.py
+++ AISamacharapp/AISamacharapp/settings.py
@@ -124,4 +124,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'profiles.CustomUser'
+#AUTH_USER_MODEL = 'profiles.CustomUser'
